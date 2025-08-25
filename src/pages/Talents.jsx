8c59d9d2199c3c1e817fe73b57183b9709461d70
stijndeschryver <stijndeschryver@icloud.com>
2025-08-25 15:00:06 +0200

    fix unused imports being treated as errors in CI

--- src/pages/Talents.jsx
+++ src/pages/Talents.jsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom'; 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'; 
 
 function Talents() {
   const [talents, setTalents] = useState([]);
